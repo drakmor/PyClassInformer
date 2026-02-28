15f15bfd666fbdadc4480e6de858e0004294b9f9
Roman Tarasov <i@drakmor.ru>
2026-02-28 14:48:23 +0300

    Persist decompilation settings across form runs

--- pyclassinformer/pci_config.py
+++ pyclassinformer/pci_config.py
@@ -5,7 +5,7 @@ except NameError:
 
 class pci_config(object):
     
-    alldata = True
+    alldata = False
     rtti = True
     exana = True
     mvvm = True
@@ -16,7 +16,27 @@ class pci_config(object):
     decomp_mode = "balanced"
     dirtree = True
     
-    def __init__(self, alldata=False, rtti=True, exana=True, mvvm=True, mvcd=True, rnvm=True, rncd=True, decomp=True, decomp_mode="balanced"):
+    def __init__(self, alldata=None, rtti=None, exana=None, mvvm=None, mvcd=None, rnvm=None, rncd=None, decomp=None, decomp_mode=None):
+        cls = type(self)
+        if alldata is None:
+            alldata = cls.alldata
+        if rtti is None:
+            rtti = cls.rtti
+        if exana is None:
+            exana = cls.exana
+        if mvvm is None:
+            mvvm = cls.mvvm
+        if mvcd is None:
+            mvcd = cls.mvcd
+        if rnvm is None:
+            rnvm = cls.rnvm
+        if rncd is None:
+            rncd = cls.rncd
+        if decomp is None:
+            decomp = cls.decomp
+        if decomp_mode is None:
+            decomp_mode = cls.decomp_mode
+
         self.alldata = alldata
         self.rtti = rtti
         self.exana = exana
@@ -29,6 +49,7 @@ class pci_config(object):
             decomp_mode = "balanced"
         self.decomp_mode = decomp_mode
         self.check_dirtree()
+        self.remember()
         
     def check_dirtree(self):
         try:
@@ -40,4 +61,16 @@ class pci_config(object):
             self.mvvm = False
             self.mvcd = False
             self.dirtree = False
-            
+
+    def remember(self):
+        cls = type(self)
+        cls.alldata = self.alldata
+        cls.rtti = self.rtti
+        cls.exana = self.exana
+        cls.mvvm = self.mvvm
+        cls.mvcd = self.mvcd
+        cls.rnvm = self.rnvm
+        cls.rncd = self.rncd
+        cls.decomp = self.decomp
+        cls.decomp_mode = self.decomp_mode
+        cls.dirtree = self.dirtree
--- pyclassinformer/pci_config_form.py
+++ pyclassinformer/pci_config_form.py
@@ -5,8 +5,9 @@ import ida_kernwin
 # --------------------------------------------------------------------------
 class pci_form_t(ida_kernwin.Form):
 
-    def __init__(self, dirtree=True):
+    def __init__(self, dirtree=True, config=None):
         self.invert = False
+        self.config = config
         F = ida_kernwin.Form
         F.__init__(
             self,
@@ -36,7 +37,7 @@ class pci_form_t(ida_kernwin.Form):
         
         # Compile (in order to populate the controls)
         self.Compile()
-        self.set_default_settings()
+        self.set_default_settings(config)
         
     def OnFormChange(self, fid):
         # set only once when it is called
@@ -46,9 +47,6 @@ class pci_form_t(ida_kernwin.Form):
         return 1
         
     def change_dirtree_settings(self):
-        self.SetControlValue(self.exana, True)
-        self.SetControlValue(self.mvvm, True)
-        self.SetControlValue(self.mvcd, True)
         if not self.dirtree:
             self.EnableField(self.exana, False)
             self.EnableField(self.mvvm, False)
@@ -56,16 +54,28 @@ class pci_form_t(ida_kernwin.Form):
             self.SetControlValue(self.exana, False)
             self.SetControlValue(self.mvvm, False)
             self.SetControlValue(self.mvcd, False)
+        else:
+            self.EnableField(self.exana, True)
+            self.EnableField(self.mvvm, True)
+            self.EnableField(self.mvcd, True)
 
-    def set_default_settings(self, decomp_mode="balanced"):
-        self.rdata.selected = True
-        self.rtti.checked = True
-        self.exana.checked = True
-        self.mvvm.checked = True
-        self.mvcd.checked = True
-        self.rnvm.checked = True
-        self.rncd.checked = True
-        self.decomp.checked = True
+    def set_default_settings(self, config=None):
+        if config is None:
+            config = self.config
+        if config is None:
+            import pyclassinformer.pci_config
+            config = pyclassinformer.pci_config.pci_config()
+
+        self.rdata.selected = not bool(getattr(config, "alldata", False))
+        self.alldata.selected = bool(getattr(config, "alldata", False))
+        self.rtti.checked = bool(getattr(config, "rtti", True))
+        self.exana.checked = bool(getattr(config, "exana", True))
+        self.mvvm.checked = bool(getattr(config, "mvvm", True))
+        self.mvcd.checked = bool(getattr(config, "mvcd", True))
+        self.rnvm.checked = bool(getattr(config, "rnvm", True))
+        self.rncd.checked = bool(getattr(config, "rncd", True))
+        self.decomp.checked = bool(getattr(config, "decomp", True))
+        decomp_mode = getattr(config, "decomp_mode", "balanced")
         if decomp_mode == "safe":
             self.decomp_safe.selected = True
         elif decomp_mode == "aggressive":
@@ -78,8 +88,7 @@ class pci_form_t(ida_kernwin.Form):
         ida_idaapi.require("pyclassinformer")
         ida_idaapi.require("pyclassinformer.pci_config")
         pcic = pyclassinformer.pci_config.pci_config()
-        f = pci_form_t(dirtree=pcic.dirtree)
-        f.set_default_settings(getattr(pcic, "decomp_mode", "balanced"))
+        f = pci_form_t(dirtree=pcic.dirtree, config=pcic)
 
         # Execute the form
         ok = f.Execute()
